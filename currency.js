3cf35178b0571ca72bbc5429bdcb622edae291ca
ljcortes11 <150732294+ljcortes11@users.noreply.github.com>
2024-02-15 13:26:34 -0600

    Create  currency.js

---
+++ currency.js
@@ -0,0 +1,19 @@
+let yuanamount = 100 * 7.19;
+
+let yenamount = 100* 150.11;
+
+let wonamount = 100 * 1332.09;
+
+
+let ch = "what do you have left in yuan? "  + yuanamount;
+
+let jp = "What do you have left in yen? " +  yenamount;
+
+let kr = "What do you have left in won? " + wonamount;
+
+let total = yuanamount + yenamount + wonamount;
+
+console.log(total)
+console.log(ch)
+console.log(jp)
+console.log(kr)
