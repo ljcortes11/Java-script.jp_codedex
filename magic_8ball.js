0cc232c8974e6c405fcf2487403e7e56c4e2f48f
ljcortes11 <150732294+ljcortes11@users.noreply.github.com>
2024-02-15 22:00:20 -0600

    Create magic_8ball.js

---
+++ magic_8ball.js
@@ -0,0 +1,24 @@
+let num = Math.floor(Math.random() * 10)
+
+
+if (num == 1){
+  console.log("Yes - definitely.")
+} else if (num == 3){
+  console.log("It is decidedly so.")
+} else if (num == 4){
+  console.log("Without a doubt.")
+} else if (num == 5){
+  console.log("Reply hazy, try again.")
+} else if (num == 6){
+  console.log("Ask again later.")
+} else if (num == 7){
+  console.log("Better not tell you now")
+} else if (num == 8){
+  console.log("My sources say no.")
+} else if (num == 9){
+  console.log("Outlook not so good.")
+} else if (num == 10){
+  console.log("Very doubtful.")
+} else {
+  console.log("Try again")
+}
